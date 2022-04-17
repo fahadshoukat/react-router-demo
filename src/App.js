bf00f9ed1434b1858d5a42b577c9f69fa77c326d
fahadshoukat <fraza6970@gmail.com>
2022-04-17 21:42:21 +0500

    add Links in navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="about" element={<About />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
